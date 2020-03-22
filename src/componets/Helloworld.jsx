79ffbd35375c9c19dfc7f14290e897d0f44170b9
Osvi <osvaldotr07@outlook.com>
2020-03-21 23:47:27 -0600

    Se configura gitignore y eslint

---
+++ src/componets/Helloworld.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../assets/style/App.scss';
+const Helloworld = () => (
+  <h1>Hola mundo</h1>
+);
+
+export default Helloworld;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Helloworld from './componets/Helloworld'
+
+ReactDOM.render(<Helloworld />, document.getElementById('app'))
